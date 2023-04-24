503a7c524d81708c83bbf7e6b19812375f66296c
frederiklindhard <frederik0203@gmail.com>
2023-04-24 21:52:18 +0200

    Vehicle model og controller oprettet

---
+++ VehicleServiceAPI/Controllers/VehicleController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace VehicleServiceAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class VehicleController : ControllerBase
+{
+
+    private readonly ILogger<VehicleController> _logger;
+    private readonly string _imagePath;
+
+    public VehicleController(ILogger<VehicleController> logger, IConfiguration config)
+    {
+        _logger = logger;
+        _imagePath = config["ImagePath"] ?? "/srv";
+    }
+
+    /*
+     * 
+    //Add new vehicle
+    [HttpPost("addVehicle")]
+    //Attach image to vehicle
+    [HttpPost("uploadImage"), DisableRequestSizeLimit]
+
+    //List vehicles info
+    [HttpGet("listImages")]
+
+    //Show vehicle info with image(s)
+    [HttpGet("listVehicleInfo")]
+    */
+}
---
+++ VehicleServiceAPI/Model/Vehicle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace VehicleServiceAPI.Model
+{
+	public class Vehicle
+	{
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string RegistrationNumber { get; set; }
+        public int Mileage { get; set; }
+        public List<string> ServiceHistory { get; set; }
+        public List<string> ImageHistory { get; set; }
+
+        public Vehicle(int id, string brand, string model, string registrationNumber, int mileage)
+        {
+            Id = id;
+            Brand = brand;
+            Model = model;
+            RegistrationNumber = registrationNumber;
+            Mileage = mileage;
+            ServiceHistory = new List<string>();
+            ImageHistory = new List<string>();
+        }
+
+        public void AddServiceRecord(string serviceRecord)
+        {
+            ServiceHistory.Add(serviceRecord);
+        }
+
+        public void AddImage(string imagePath)
+        {
+            ImageHistory.Add(imagePath);
+        }
+    }
+}
+
