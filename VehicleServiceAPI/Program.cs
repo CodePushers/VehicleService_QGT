9f35c34e1f94ef05381d6535496b58bac5eb6983
Anders <90785699+lynge96@users.noreply.github.com>
2023-04-25 11:41:48 +0200

    Slettet img kode

--- VehicleServiceAPI/Program.cs
+++ VehicleServiceAPI/Program.cs
@@ -11,19 +11,6 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Setup application to handle download of static content (images)
-// from a predetermined folder
-var imagePath = builder.Configuration["ImagePath"];
-var fileProvider = new PhysicalFileProvider(Path.GetFullPath(imagePath));
-var requestPath = new PathString("/images");
-app.UseStaticFiles(new StaticFileOptions()
-{
-    FileProvider = fileProvider,
-    RequestPath = requestPath
-});
-Console.WriteLine("File Provider Root: " + fileProvider.Root);
-Console.WriteLine("requestPath" + requestPath);
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
