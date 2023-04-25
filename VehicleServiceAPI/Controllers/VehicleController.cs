96e4e509fceba3db24abb35c17e51e9aec7603d7
Anders <90785699+lynge96@users.noreply.github.com>
2023-04-25 09:42:34 +0200

    Mongo Integration + dockerfile

--- VehicleServiceAPI/Controllers/VehicleController.cs
+++ VehicleServiceAPI/Controllers/VehicleController.cs
@@ -1,4 +1,8 @@
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System.Text;
 
 namespace VehicleServiceAPI.Controllers;
 
@@ -6,14 +10,30 @@ namespace VehicleServiceAPI.Controllers;
 [Route("[controller]")]
 public class VehicleController : ControllerBase
 {
-
     private readonly ILogger<VehicleController> _logger;
+    private readonly IConfiguration _config;
     private readonly string _imagePath;
+    private readonly IMongoCollection<Vehicle> _vehicles;
+
 
     public VehicleController(ILogger<VehicleController> logger, IConfiguration config)
     {
         _logger = logger;
+        _config = config;
+
         _imagePath = config["ImagePath"] ?? "/srv";
+
+        // Client
+        var mongoClient = new MongoClient(_config["ConnectionURI"]);
+        _logger.LogInformation($"[*] CONNECTION_URI: {_config["ConnectionURI"]}");
+
+        // Database
+        var database = mongoClient.GetDatabase(_config["DatabaseName"]);
+        _logger.LogInformation($"[*] DATABASE: {_config["DatabaseName"]}");
+
+        // Collection
+        _vehicles = database.GetCollection<Vehicle>(_config["CollectionName"]);
+        _logger.LogInformation($"[*] COLLECTION: {_config["CollectionName"]}");
     }
 
     /*
